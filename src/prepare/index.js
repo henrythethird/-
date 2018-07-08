3fb47c4f049d38eeb5ad55d4abf25c264f9467db
Patrick Kaufmann <patrick.kaufmann@pwc-digital.ch>
2018-07-08 16:23:14 +0200

    Added articles

--- src/prepare/index.js
+++ src/prepare/index.js
@@ -8,9 +8,14 @@ const rd = fs.readdirSync(blogPath).filter((file) => {
         return file.endsWith('.html')
     }).map((file) => {
         const name = file.substr(11, file.length - 16)
+        const date = file.substr(0, 10)
+        
+        // eslint-disable-next-line
+        console.log('Writing "' + name + '" from ' + date)
 
         return {
             name,
+            date,
             content: fs.readFileSync(path.join(blogPath, file), "utf8")
         }
     }).reverse()
